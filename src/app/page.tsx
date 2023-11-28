dd345deb1ee3d35c4919547d3527600ee0e1f4f3
minhaj-boop <imminhajj@gmail.com>
2023-11-29 00:24:57 +0600

    slider complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+import Slider from "@/components/Slider";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Hello world</main>;
+  return (
+    <main>
+      <Slider />
+      <Featured />
+      <Offer />
+    </main>
+  );
 }
