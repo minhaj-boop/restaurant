97aacc01c0db4e2bebee33c042d835ee55034e16
minhaj-boop <imminhajj@gmail.com>
2024-01-03 20:18:23 +0600

    cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
 import React from "react";
 
 const CartPage = () => {
-  return <div>CartPage</div>;
+  return (
+    <div className="">
+      {/* PRODUCT CONTAINER */}
+      <div className="">
+        {/* SINGLE ITEM */}
+        <div className="">
+          <Image src="/temporary/p1.png" alt="" fill />
+          <div className="">
+            <h1>sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2>$79.90</h2>
+          <span>X</span>
+        </div>
+      </div>
+      {/* PAYMENT CONTAINER */}
+      <div className="">
+        <div className="">
+          <span className="">Subtotal (3 items)</span>
+          <span className="">$81.70</span>
+        </div>
+        <hr className="" />
+        <button className="">CHECKOUT</button>
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
