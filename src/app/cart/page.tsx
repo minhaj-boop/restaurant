7ee72d6d789bb17f90f2765786dca82e291a0388
minhaj-boop <imminhajj@gmail.com>
2024-01-04 00:07:46 +0600

    cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,28 +3,30 @@ import React from "react";
 
 const CartPage = () => {
   return (
-    <div className="">
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500">
       {/* PRODUCT CONTAINER */}
-      <div className="">
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll">
         {/* SINGLE ITEM */}
-        <div className="">
-          <Image src="/temporary/p1.png" alt="" fill />
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
           <div className="">
-            <h1>sicilian</h1>
+            <h1 className="text-xl uppercase font-bold">sicilian</h1>
             <span>Large</span>
           </div>
-          <h2>$79.90</h2>
-          <span>X</span>
+          <h2 className="font-bold">$79.90</h2>
+          <span className="cursor-pointer">X</span>
         </div>
       </div>
       {/* PAYMENT CONTAINER */}
-      <div className="">
+      <div className="h-1/2 p-4 bg-fuchsia-50">
         <div className="">
           <span className="">Subtotal (3 items)</span>
           <span className="">$81.70</span>
         </div>
         <hr className="" />
-        <button className="">CHECKOUT</button>
+        <button className="bg-red-500 text-white p-3 rounded-md w-1/2">
+          CHECKOUT
+        </button>
       </div>
     </div>
   );
